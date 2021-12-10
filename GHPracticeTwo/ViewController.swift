dcb0e33ae4f09604c6ae9c19b4238a8ece07d755
stephanmarkovski <smarkovski4271@stu.d214.org>
2021-12-10 10:22:33 -0600

    added tableview items

--- GHPracticeTwo/ViewController.swift
+++ GHPracticeTwo/ViewController.swift
@@ -7,11 +7,25 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return array.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "myCell", for: indexPath)
+        cell.textLabel?.text = array[indexPath.row]
+        
+        return cell
+    }
+    
+    @IBOutlet weak var tableView: UITableView!
+    var array = ["bear", "penguin", "pig"]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.delegate = self
     }
 
 
